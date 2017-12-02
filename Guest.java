7e7cfcbfe33d8c9532148ac7db330c4087edf664
danemortensen <mortensendane@gmail.com>
2017-12-02 15:47:50 -0800

    Guest

--- Guest.java
+++ Guest.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 public class Guest extends User {
     private static final String type = "user";
 
-    String roomQuery = "SELECT RoomName FROM rooms;"
+    public static void roomRates() {
+        Scanner s = new Scanner();
 
-
+        String roomQuery = "SELECT RoomName FROM rooms;";
+        System.out.println("Select room from list: ");
+        
+    }
 }
