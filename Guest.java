b04b87fe61faf940addc801b28ac33dd95dfc451
danemortensen <mortensendane@gmail.com>
2017-12-02 18:13:37 -0800

    startup completed

--- Guest.java
+++ Guest.java
@@ -4,10 +4,6 @@ public class Guest extends User {
     private static final String type = "user";
 
     public static void roomRates() {
-        Scanner s = new Scanner();
-
-        String roomQuery = "SELECT RoomName FROM rooms;";
-        System.out.println("Select room from list: ");
         
     }
 }
